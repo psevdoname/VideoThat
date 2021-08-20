c31712a61174967e22947b1d4c184610b1802b7b
Artem Dovzhenko <temadovzh@gmail.com>
2021-08-20 23:28:01 +0300

    dD

--- scenedetect_all.sh
+++ scenedetect_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-find data -type f \( -name '*mkv' -o  -name '*webbm' \) \
+find data -type f \( -name '*mkv' \) \
      -exec scenedetect --drop-short-scenes -m 1.3s --input {} --output data/taylor --stats {}.stats.csv detect-content --threshold 15 list-scenes \; 
